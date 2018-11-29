59476ac2ccbe50a866dd7f5d653d98c96f93c3a4
Joe Blau <josephblau@gmail.com>
2018-11-29 07:30:52 -0500

    Update Moban to sync all of the new templates and directories added by GitHub

--- .travis/sync-github.sh
+++ .travis/sync-github.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-git clone --depth 1 https://github.com/github/gitignore ../github
-find ../github -name '*.gitignore' | xargs sed -i 's/ *$//g;$a\'
+git clone --depth 1 https://github.com/github/gitignore ../temp-github-gitignore
+find ../temp-github-gitignore -name '*.gitignore' | xargs sed -i 's/ *$//g;$a\'
