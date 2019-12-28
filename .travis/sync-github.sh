2cf6c56e0c3ed78b9cae5174b52c885e358e69fd
Joe Blau <josephblau@gmail.com>
2019-12-28 05:51:40 -0800

    Remove travis to switch over to GH actions

--- .travis/sync-github.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-git clone --depth 1 https://github.com/github/gitignore ../temp-github-gitignore
-find ../temp-github-gitignore -type f -print0 | xargs -0 xattr -c
-find ../temp-github-gitignore -name '*.gitignore' | xargs sed -i 's/ *$//g;$a\'
