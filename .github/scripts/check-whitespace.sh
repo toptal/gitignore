dda78d670fe19bb3087018e4dc2260312ac44ecb
Filipe Chagas <filipe.chagas@toptal.com>
2021-06-18 08:21:07 -0300

    Fix github scripts
    Update from github gitignore
    Fix EOF instead of just informing the problem
    Invert moban and whitespace scripts
    Update README so contributors won't bump into checker failures

--- .github/scripts/check-whitespace.sh
+++ .github/scripts/check-whitespace.sh
@@ -3,12 +3,16 @@
 for file in templates/*; do
     if [[ -f $file && ! -z "$(tail -c 1 "$file")" ]]; then
         echo "File '$file' is missing EOF line"
-
-        exit 1
+        echo "Fixing '$file'"
+        sed -i -e '$a\' $file
+        echo "'$file' fixed"
     fi
 done
 
+echo "Removing extra spaces"
 find templates/ -type f | xargs sed -i 's/ *$//'
+
+echo "Checking for uncommitted changes"
 git diff --exit-code
 
 exit 0
