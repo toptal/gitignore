16bb5aeb8b34826c564b6c3c8dd999d1348bb31b
Konstantin Rybakov <konstantin.rybakov@toptal.com>
2022-01-11 11:32:30 +0300

    Update sync-github.sh

--- .github/scripts/sync-github.sh
+++ .github/scripts/sync-github.sh
@@ -2,5 +2,5 @@
 
 rm -rf ../temp-github-gitignore
 git clone --depth 1 https://github.com/github/gitignore ../temp-github-gitignore
-# find ../temp-github-gitignore -type f -name '*.gitignore' -print0 | xargs -0 xattr -c
+find ../temp-github-gitignore -type f -name '*.gitignore' -print0 | xargs -0 xattr -c
 find ../temp-github-gitignore -name '*.gitignore' | xargs sed -i 's/ *$//g;$a\'
